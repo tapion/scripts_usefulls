180b4af83fee009eb14819326b91f6261bacaa8f
Miguel Vargas <tapion@Miguels-MacBook-Pro.local>
2023-02-27 10:36:23 -0500

    Se mejora el ejemplo con promesas y agrego que cola se encarga de que tarea

--- event_loop_nodejs.js
+++ event_loop_nodejs.js
@@ -45,13 +45,13 @@
 // console.log('script end'); //5
 
 ///EJERCICIO NUEVO
-Promise.resolve().then(() => console.log('promise1 callback')); // 1
+Promise.resolve().then(() => console.log('promise1 callback')); // 1 (Microtask queue)
 
-Promise.resolve().then(() => { // 2
+Promise.resolve().then(() => { // 2 (Microtask queue)
 
     console.log('promise2 callback'); // 2.1
 
-    process.nextTick(() => console.log('next tick inside promise2')); // 2.2
+    process.nextTick(() => console.log('next tick inside promise2')); // 2.2 (Nextick queue)
 
 });
 Promise.resolve().then(() => { //3
@@ -59,20 +59,28 @@ Promise.resolve().then(() => { //3
     console.log('promise2  de 2 callback'); // 3.1
 
 });
-console.log("hola"); // 4
 
-setImmediate(() => console.log('immediate1 callback')); //5
+console.log("hola"); // 4 (Stack execution)
 
-setImmediate(() => console.log('immediate2 callback')); //6
+setImmediate(() => console.log('immediate1 callback')); //5 (Inmediate queue)
 
-process.nextTick(() => console.log('next tick1 callback')); //7
+setImmediate(() => console.log('immediate2 callback')); //6 (Inmediate queue)
 
-process.nextTick(() => console.log('next tick2 callback')); //8
+process.nextTick(() => console.log('next tick1 callback')); //7 (Nextick queue)
 
-setTimeout(() => console.log('set timeout'), 0); // 9
-setTimeout(() => console.log('set timeout1'), 0); // 10
-setTimeout(() => console.log('set timeout2'), 0); // 11
+process.nextTick(() => console.log('next tick2 callback')); //8 (Nextick queue)
 
-setImmediate(() => console.log('immediate3 callback')); // 12
+setTimeout(() => console.log('set timeout'), 0); // 9 (Timer queue)
+setTimeout(() => console.log('set timeout1'), 0); // 10 (Timer queue)
+setTimeout(() => console.log('set timeout2'), 0); // 11 (Timer queue)
 
-//Orden ejecucion: 4, 7, 8, 1, 2, 2.1, 3, 3.1, 2.2, 9, 10, 11, 5, 6, 12
+setImmediate(() => console.log('immediate3 callback')); // 12 (Inmediate queue)
+
+const promesa = new Promise((resolve, reject) => { //13 (Stack execution)
+    console.log("En la promesa"); //13.5
+    return resolve(9)
+});
+
+promesa.then(val => console.log('Valor de la promesa', val)); //14 (Microtask queue)
+
+//Orden ejecucion: 4, 13.5, 7, 8, 1, 2, 2.1, 3, 3.1, 14, 2.2, 9, 10, 11, 5, 6, 12
